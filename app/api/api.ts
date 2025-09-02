6a2f9ff79c55b81fa6f807abce02dfc635375d24
wailTz55 <wailziadtarek@gmail.com>
2025-09-02 11:10:25 +0100

    fixe3:add the baseURL in contact,register and api.ts files

--- app/api/api.ts
+++ app/api/api.ts
@@ -3,8 +3,8 @@ import { AllDataResponse } from "./type";
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function fetchAllData(): Promise<AllDataResponse> {
-    // const response = await fetch(`${baseURL}/api/all-data/`);
-    const response = await fetch(`https://almanr.pythonanywhere.com/api/all-data/`);
+    const response = await fetch(`${baseURL}/api/all-data/`);
+    // const response = await fetch(`https://almanr.pythonanywhere.com/api/all-data/`);
     if (!response.ok) {
         throw new Error("فشل في جلب البيانات");
     }
